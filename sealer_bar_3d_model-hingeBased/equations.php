88fbdc2c54e24fdb86358aa6ba6f3b752ffc2729
Neil Jackson <neiljackson1984@gmail.com>
2017-09-25 14:14:27 -0700

    added springs.

--- sealer_bar_3d_model-hingeBased/equations.php
+++ sealer_bar_3d_model-hingeBased/equations.php
@@ -20,20 +20,29 @@ $mainBar->extentX = $heaterBed->extentX;
 $mainBar->extentZ = $heaterBed->extentZ + 100 * $millimeter;
 $mainBar->extentY = 70 * $millimeter;
 $mainBar->foot->extentY = 15 * $millimeter;
+$mainBar->pivotInsetZ = 30 * $millimeter;
+$mainBar->pivotInsetY = 7 * $millimeter;
+
 
 $arm->sidePlate->extentX = 3 * $millimeter; //stock sheet thickness
-$arm->sidePlate->extentY = 65 * $millimeter;
+
 $arm->sidePlate->extentZ = 10 * $millimeter;
-$arm->sidePlatesIntervalX = $mainBar->extentX + $arm->sidePlate->extentX;
+$arm->sidePlate->pivotDedendum = 4 * $millimeter;
+$arm->sidePlate->extentY = $arm->sidePlate->pivotDedendum + $mainBar->extentY  - $mainBar->pivotInsetY;
+
 
 $arm->endBlock->extentX = $heaterBed->extentX;
 $arm->endBlock->extentY = 20 * $millimeter;
 $arm->endBlock->extentZ = 9 * $millimeter;
 
+$arm->pivotAxisRootPoint->position->y = 0;
+$arm->endBlockPosition->y = $arm->pivotAxisRootPoint->position->y + $mainBar->extentY  - $mainBar->pivotInsetY - $arm->endBlock->extentY/2;
+
+$pivotShaft->diameter = 5 * $millimeter;
+$pivotShaft->holeDiameter = $pivotShaft->diameter + 0.02 * $millimeter;
+$pivotShaft->length = $mainBar->extentX + 2*$arm->sidePlate->extentX + 10 * $millimeter;
 
 
-$mainBar->pivotInsetZ = 30 * $millimeter;
-$mainBar->pivotInsetY = 7 * $millimeter;
 
 
 ?>
