f3d30f0e6efe74cd6326c651dfdd5210ea5a1074
Neil Jackson <neiljackson1984@gmail.com>
2017-10-08 14:45:18 -0700

    checkpoint

--- sealer_bar_3d_model-hingeBased/equations.php
+++ sealer_bar_3d_model-hingeBased/equations.php
@@ -10,7 +10,12 @@ $mm = $millimeter;
 $degree = 1;
 $radian = 180/pi();
 
+function average($arr)
+{
+   if (!is_array($arr)) return false;
 
+   return array_sum($arr)/count($arr);
+}
 
 
 // the heaterBed is a right-rectangular flat surface that is the aluminum surface lying below and supporting the heating element.
@@ -33,25 +38,42 @@ $arm->sidePlate->extent->y = $arm->sidePlate->pivotDedendum + $mainBar->extent->
 
 
 $arm->endBlock->extent->x = $heaterBed->extent->x;
-$arm->endBlock->extent->y = 20 * $millimeter;
 $arm->endBlock->extent->z = 9 * $millimeter;
 
 $arm->pivotAxisRootPoint->position->y = 0;
-$arm->endBlockPosition->y = $arm->pivotAxisRootPoint->position->y + $mainBar->extent->y  - $mainBar->pivotInsetY - $arm->endBlock->extent->y/2;
+
 
 $pivotShaft->diameter = 5 * $millimeter;
 $pivotShaft->holeDiameter = $pivotShaft->diameter + 0.02 * $millimeter;
 $pivotShaft->length = $mainBar->extent->x + 2*$arm->sidePlate->extent->x + 10 * $millimeter;
 
-
+$arm->endBlockYMax = $arm->pivotAxisRootPoint->position->y + $mainBar->extent->y  - $mainBar->pivotInsetY;
 
 $arm->viseScrew = new screw("M5-0.8");
+$arm->viseScrew->length = 14 * $mm;
+
+$arm->viseScrew->clampingDiameter = 10 * $mm;
 $arm->viseClampingZone->extent->x = 5 * $mm;
-$arm->viseScrews->intervalX = $arm->viseClampingZone->extent->x  + $arm->viseScrew->freeFitDiameter;
+$arm->viseScrews->intervalX = max([$arm->viseClampingZone->extent->x + $arm->viseScrew->freeFitDiameter, $arm->viseScrew->clampingDiameter]);
 
 $arm->viseBar->extent->x = $arm->viseScrews->intervalX + $arm->viseScrew->clampingDiameter;//$arm->endBlock->extent->x;
+
+$arm->viseBarYMax = $arm->endBlockYMax - 5 * $mm;
 $arm->viseBar->extent->y = $arm->viseScrew->clampingDiameter;
+$arm->viseBarYMin = $arm->viseBarYMax - $arm->viseBar->extent->y;
+
 $arm->viseBar->extent->z = 6 * $mm;
 
+$arm->viseBarPosition->y = average([$arm->viseBarYMax, $arm->viseBarYMin]);
 
+$arm->terminalScrew = new screw("M5-0.8");
+$arm->terminalScrew->length = 10* $mm;
+$arm->terminalClampingDiameter = 14 * $mm;
+$arm->terminalScrewPosition->y = $arm->viseBarYMin - $arm->terminalClampingDiameter/2;
+
+
+$arm->endBlockYMin = $arm->terminalScrewPosition->y - $arm->terminalClampingDiameter/2;
+$arm->endBlockPosition->y = average([$arm->endBlockYMax, $arm->endBlockYMin]);
+
+$arm->endBlock->extent->y = $arm->endBlockYMax - $arm->endBlockYMin;
 ?>
